f67e779d73c79279aba75429c15b9d3aee355bf0
Ashish Hans Catada Ghimire <93694081+ToastedCake@users.noreply.github.com>
2023-02-20 17:13:12 -0800

    added weather and location
    :+1:

---
+++ src/components/GeolocationButton.tsx
@@ -0,0 +1,47 @@
+import { Geolocation, Geoposition } from '@ionic-native/geolocation';
+import { IonButton, IonLoading, IonToast } from '@ionic/react';
+import React, { useState } from 'react';
+
+interface LocationError {
+    showError: boolean;
+    message?: string;
+}
+
+const GeolocationButton: React.FC = () => {
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<LocationError>({ showError: false });
+    const [position, setPosition] = useState<Geoposition>();
+
+    const getLocation = async () => {
+        setLoading(true);
+
+        try {
+            const position = await Geolocation.getCurrentPosition();
+            setPosition(position);
+            setLoading(false);
+            setError({ showError: false });
+        } catch (e) {
+            setError({ showError: true, message: (e as Error).message });
+            setLoading(false);
+        }
+    }
+
+    return (
+        <>
+            <IonLoading
+                isOpen={loading}
+                onDidDismiss={() => setLoading(false)}
+                message={'Getting Location...'}
+            />
+            <IonToast
+                isOpen={error.showError}
+                onDidDismiss={() => setError({ message: "", showError: false })}
+                message={error.message}
+                duration={3000}
+            />
+            <IonButton color="primary" onClick={getLocation}>{position ? `i did it 🤡🤡🤡🤡🤡🤡🤡 ${position.coords.latitude} ${position.coords.longitude}` : "Get Location"}</IonButton>
+        </>
+    );
+};
+
+export default GeolocationButton;
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,36 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonGrid,
+  IonCol,
+  IonRow,
+} from "@ionic/react";
+import React from "react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Tab1.css";
+import GeolocationButton from "../components/GeolocationButton";
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Geolocation</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
+      <IonContent>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Geolocation</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+
+        <div className="geo-button">
+          <GeolocationButton />
+        </div>
       </IonContent>
     </IonPage>
   );
