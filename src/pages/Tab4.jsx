59e0118bf099b5cdd9c91d01d5593bd8c4c12988
Ashish Hans Catada Ghimire <93694081+ToastedCake@users.noreply.github.com>
2023-02-23 15:48:01 -0800

    Update Tab4.jsx

--- src/pages/Tab4.jsx
+++ src/pages/Tab4.jsx
@@ -6,13 +6,10 @@ import { IonPage } from "@ionic/react";
 function Tab4() {
   const [data, setData] = useState([]);
   const fetchData = async () => {
-    // fetch(`https://dummyjson.com/products`)
     const position = await Geolocation.getCurrentPosition();
     var radius = "5";
     var count = "3";
-    // https://justcors.com expires at midnight lol
-    var cors = "https://justcors.com/tl_85f1dc7/"
-    var url = cors + "https://delightful-mushroom-f6ea281114064ec7a6bd48c7ad707e18.azurewebsites.net/nearby-trails?lat=" + position.coords.latitude.toString() + "&long=" + position.coords.longitude.toString() + "&radius=" + radius + "&count=" + count;
+    var url = "https://delightful-mushroom-f6ea281114064ec7a6bd48c7ad707e18.azurewebsites.net/nearby-trails?lat=" + position.coords.latitude.toString() + "&long=" + position.coords.longitude.toString() + "&radius=" + radius + "&count=" + count;
 
     fetch (url)
     // fetch ("https://justcors.com/tl_85f1dc7/https://delightful-mushroom-f6ea281114064ec7a6bd48c7ad707e18.azurewebsites.net/nearby-trails?lat=60.18852&long=-149.63156")
@@ -86,56 +83,3 @@ function Tab4() {
 }
 
 export default Tab4;
-
-
-// // App.js
-// import { useEffect, useState } from "react";
-// import "./App.css";
-
-// function App() {
-//   const [data, setData] = useState([]);
-
-//   const fetchData = () => {
-//     fetch(`https://dummyjson.com/products`)
-//       .then((response) => response.json())
-//       .then((actualData) => {
-//         console.log(actualData);
-//         setData(actualData.products);
-//         console.log(data);
-//       })
-//       .catch((err) => {
-//         console.log(err.message);
-//       });
-//   };
-
-//   useEffect(() => {
-//     fetchData();
-//   }, []);
-
-//   return (
-//     <div className="App">
-//       <tbody>
-//         <tr>
-//           <th>Name</th>
-//           <th>Brand</th>
-//           <th>Image</th>
-//           <th>Price</th>
-//           <th>Rating</th>
-//         </tr>
-//         {data.map((item, index) => (
-//           <tr key={index}>
-//             <td>{item.title}</td>
-//             <td>{item.brand}</td>
-//             <td>
-//               <img src={item.thumbnail} alt="" height={100} />
-//             </td>
-//             <td>{item.price}</td>
-//             <td>{item.rating}</td>
-//           </tr>
-//         ))}
-//       </tbody>
-//     </div>
-//   );
-// }
-
-// export default App;
