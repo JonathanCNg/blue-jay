5f09ba05da09b6a119e60ea646ec4d879838471f
Ashish Hans Catada Ghimire <93694081+ToastedCake@users.noreply.github.com>
2023-02-22 18:22:12 -0800

    Questionaire for user
    questionaire for user?

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,4 +1,6 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React from 'react';
+import { IonInput, IonItem, IonLabel } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
 
@@ -16,7 +18,25 @@ const Tab3: React.FC = () => {
             <IonTitle size="large">Tab 3</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+          <IonItem fill = "outline">
+            <IonLabel position="floating">What is your name?</IonLabel>
+            <IonInput placeholder="Enter text"></IonInput>
+          </IonItem>
+
+          <IonItem fill = "outline">
+            <IonLabel position="floating">What is your age?</IonLabel>
+            <IonInput type="number" placeholder="000"></IonInput>
+          </IonItem>    
+
+          <IonItem fill = "outline">
+            <IonLabel position="floating">What is your height?</IonLabel>
+            <IonInput type="number" placeholder="000"></IonInput>
+          </IonItem>
+
+          <IonItem fill = "outline">
+            <IonLabel position="floating">What is your weight?</IonLabel>
+            <IonInput type ="number" placeholder="000"></IonInput>
+          </IonItem>
       </IonContent>
     </IonPage>
   );
