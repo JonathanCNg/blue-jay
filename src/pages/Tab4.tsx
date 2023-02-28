75c6592f75a17252744cd515a23d87600bd15746
yocelynh <yocelynh@uci.edu>
2023-02-27 19:59:22 -0800

    Added trail preferences survey

--- src/pages/Tab4.tsx
+++ src/pages/Tab4.tsx
@@ -9,7 +9,7 @@ const Tab4: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 4</IonTitle>
+          <IonTitle>Profile</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -39,7 +39,7 @@ const Tab4: React.FC = () => {
           </IonItem>
 
           <IonItem>
-            <IonLabel>Select Fitness Level</IonLabel>
+            <IonLabel>Fitness Level</IonLabel>
             <IonSelect placeholder="Make a Selection" multiple={true}>
               <IonSelectOption value="beg">Beginner</IonSelectOption>
               <IonSelectOption value="int">Intermediate</IonSelectOption>
@@ -48,7 +48,53 @@ const Tab4: React.FC = () => {
               <IonCheckbox slot="end"></IonCheckbox>
             </IonSelect>
           </IonItem>
-          
+
+          <IonItem>
+            <IonLabel>Favorite Trail Views</IonLabel>
+            <IonSelect placeholder="Make a Selection" multiple={true}>
+              <IonSelectOption value="beach">Beach</IonSelectOption>
+              <IonSelectOption value="lake">Lake</IonSelectOption>
+              <IonSelectOption value="water">Waterfall</IonSelectOption>
+              <IonSelectOption value="city">City</IonSelectOption>
+              <IonSelectOption value="wild">Wildlife</IonSelectOption>
+              <IonSelectOption value="forest">Forest</IonSelectOption>
+              <IonSelectOption value="historic">Historic Sites</IonSelectOption>
+              <IonCheckbox slot="end"></IonCheckbox>
+            </IonSelect>
+          </IonItem>
+
+
+          <IonItem>
+            <IonLabel>Favorite Trail Activites</IonLabel>
+            <IonSelect placeholder="Make a Selection" multiple={true}>
+              <IonSelectOption value="Walking">Walking</IonSelectOption>
+              <IonSelectOption value="Biking">Biking</IonSelectOption>
+              <IonSelectOption value="Kayaking">Kayaking</IonSelectOption>
+              <IonSelectOption value="Rock Climbing">Rock Climbing</IonSelectOption>
+              <IonSelectOption value="Canoeing">Canoeing</IonSelectOption>
+              <IonSelectOption value="Horseback riding">Horseback riding</IonSelectOption>
+              <IonSelectOption value="Hiking">Hiking</IonSelectOption>
+              <IonSelectOption value="Backpaking">Backpaking</IonSelectOption>
+              <IonSelectOption value="Fishing">Fishing</IonSelectOption>
+              <IonSelectOption value="Paddle Boarding">Paddle Boarding</IonSelectOption>
+              <IonSelectOption value="Bird Watching">Bird Watching</IonSelectOption>
+              <IonSelectOption value="Mountain Biking">Mountain Biking</IonSelectOption>
+              <IonSelectOption value="Surfing">Surfing</IonSelectOption>
+              <IonSelectOption value="Camping">Camping</IonSelectOption>
+              <IonCheckbox slot="end"></IonCheckbox>
+            </IonSelect>
+          </IonItem>
+
+          <IonItem>
+            <IonLabel>Favorite Route Types</IonLabel>
+            <IonSelect placeholder="Make a Selection" multiple={true}>
+              <IonSelectOption value="loop">Loop</IonSelectOption>
+              <IonSelectOption value="out/back">Out and Back</IonSelectOption>
+              <IonSelectOption value="point">Point to Point</IonSelectOption>
+              <IonCheckbox slot="end"></IonCheckbox>
+            </IonSelect>
+          </IonItem>
+
       </IonContent>
     </IonPage>
   );
