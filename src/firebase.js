b3b2147d8629253b5ef5f8fdca6505bed48f70d2
hackerrank <jonng1234@gmail.com>
2023-02-28 15:34:11 -0800

    Update firebase.js
    Co-Authored-By: Ashish Hans Catada Ghimire <93694081+ToastedCake@users.noreply.github.com>

--- src/firebase.js
+++ src/firebase.js
@@ -9,14 +9,13 @@ import { getFirestore } from "firebase/firestore"
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBn9Hm4nHrwBruQ85UZ3hOA85N6dNFdV-k",
-  authDomain: "compsci-125.firebaseapp.com",
-  databaseURL: "https://compsci-125-default-rtdb.firebaseio.com",
-  projectId: "compsci-125",
-  storageBucket: "compsci-125.appspot.com",
-  messagingSenderId: "153650425969",
-  appId: "1:153650425969:web:a1daec6ffd1831f221ebd0",
-  measurementId: "G-1DBF9QXCBM"
+  apiKey: "AIzaSyBBsag1fMZHQgkGtM1bfKPYa04DalfLTtg",
+  authDomain: "bluejay-cf7c1.firebaseapp.com",
+  projectId: "bluejay-cf7c1",
+  storageBucket: "bluejay-cf7c1.appspot.com",
+  messagingSenderId: "21401365321",
+  appId: "1:21401365321:web:ce9c60ca557d67b99ec898",
+  measurementId: "G-8D13M5SW81"
 };
 
 
