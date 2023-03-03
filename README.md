f6a4ad8dfb381f6162273333f648be35cbb1f2d8
Ashish Hans Catada Ghimire <93694081+ToastedCake@users.noreply.github.com>
2023-03-02 17:33:11 -0800

    added reading and writing to database

--- README.md
+++ README.md
@@ -3,8 +3,6 @@ npm i -g @ionic/cli
 npm install
 npm install cordova-plugin-geolocation
 npm install @ionic-native/geolocation
-npm install --save @react-native-firebase/app @react-native-firebase/firestore
-npm install firebase
 ionic cap sync
 ionic serve
 ```
---
+++ delete_me.js
@@ -0,0 +1,12 @@
+import { getDatabase } from "firebase/database";
+
+// Get a database reference to our posts
+const db = getDatabase();
+const ref = db.ref('server/saving-data/fireblog/posts');
+
+// Attach an asynchronous callback to read the data at our posts reference
+ref.on('value', (snapshot) => {
+  console.log(snapshot.val());
+}, (errorObject) => {
+  console.log('The read failed: ' + errorObject.name);
+}); 
