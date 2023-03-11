f13b20970f5e11c9da0d400049ccd2a130f2ce68
hackerrank <jonng1234@gmail.com>
2023-03-10 16:10:44 -0800

    Update App.tsx to hide tab 1 and 4. Changed tab names and icons

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,8 @@ import {
   IonTabs,
   setupIonicReact
 } from '@ionic/react';
-import { logoIonic, personOutline } from 'ionicons/icons';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { personSharp, homeSharp, trailSignSharp } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
@@ -65,25 +64,25 @@ const App: React.FC = () => (
           </Route>
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
+          {/* <IonTabButton tab="tab1" href="/tab1">
             <IonIcon icon={triangle} />
             <IonLabel>Tab 1</IonLabel>
-          </IonTabButton>
+          </IonTabButton> */}
           <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
+            <IonIcon icon={homeSharp} />
+            <IonLabel>My City</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
+            <IonIcon icon={trailSignSharp} />
+            <IonLabel>Trails</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="tab4" href="/tab4">
+          {/* <IonTabButton tab="tab4" href="/tab4">
             <IonIcon icon={personOutline} />
             <IonLabel>Profile</IonLabel>
-          </IonTabButton>
+          </IonTabButton> */}
           <IonTabButton tab="tab5" href="/tab5">
-            <IonIcon icon={personOutline} />
-            <IonLabel>Test</IonLabel>
+            <IonIcon icon={personSharp} />
+            <IonLabel>Profile</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
